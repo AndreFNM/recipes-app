515ac6690fbd220d3ba9603383dc921fcd9d25c4
AndreFNM <2002andrefilipe@gmail.com>
2025-01-06 16:50:12 +0000

    Gif for readme

--- components/CreateNewRecipeBtn.tsx
+++ components/CreateNewRecipeBtn.tsx
@@ -13,7 +13,7 @@ export default function CreateNewRecipeBtn(): JSX.Element {
     <button
       data-testid="newRecipeBtn"
       onClick={handleBtn}
-      className="bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-6 sm:px-8 rounded inline-flex items-center transform hover:scale-105 transition-transform duration-300 ease-in-out"
+      className="bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-6 sm:px-8 rounded inline-flex items-center transform hover:scale-105 transition-transform duration-300 ease-in-out mt-8"
     >
       <span>Create New Recipe</span>
     </button>
